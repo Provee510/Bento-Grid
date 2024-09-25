838d1c946392597c50cfa2d71dffac512a187e7a
provee510 <providencecharles@yahoo.com>
2024-09-25 06:31:22 -0700

    workings

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
 
       {/* Right side - 83% */}
       <div className="w-[50%] bg-gray-100 p-4 h-full flex flex-col gap-3 relative right-5 items-stretch ">
-        <div className="flex gap-3 relative top-3 mb-4">
+        <div className="flex gap-3 relative top-3 ">
           <Rectangle/>
           <Single />
         </div>
